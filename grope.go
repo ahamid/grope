e7bb7ba5fdc2e888aeaf741e2aa3a32a359c2edc
Aaron Hamid <aaron.hamid@gmail.com>
2012-07-07 19:18:08 -0400

    fix arg handling

--- grope.go
+++ grope.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-  regexp := regexp.MustCompile(os.Args[1])
+  var re *regexp.Regexp
   var b []byte
   var out *os.File
   var replacement []byte
@@ -24,6 +24,8 @@ func main() {
     args = args[1:]
   }
 
+  re= regexp.MustCompile(args[1])
+ 
   if len(args) > 3 {
     replacement = []byte(args[2])
     infile = &args[3]
@@ -43,16 +45,26 @@ func main() {
   }
 
   if infile != nil && inplace {
-    out, _ = os.OpenFile(*infile, os.O_WRONLY, 644)
+    os.Stderr.Write([]byte("Setting out to " + *infile + "\n"))
+    out, err = os.OpenFile(*infile, os.O_WRONLY, 644)
+    if (err != nil) {
+      os.Stderr.Write([]byte("Error setting outputfile"))
+    }
   }
 
+  os.Stderr.Write([]byte(args[1]))
   if replacement != nil {
-    out.Write(regexp.ReplaceAll(b, replacement))
+    os.Stderr.Write([]byte("WRITING REPLACEMENT " + string(replacement) + "\n"))
+    os.Stderr.Write(b)
+    os.Stderr.Write(re.ReplaceAll(b, replacement))
+    out.Write(re.ReplaceAll(b, replacement))
   } else {
-    result := regexp.FindAll(b, 100)
+    result := re.FindAll(b, 100)
     for _, match := range result {
+      os.Stderr.Write([]byte("Writing match " + string(match)))
       out.Write(match)
       out.Write([]byte("\n"))
     }
   }
+  out.Sync()
 }
