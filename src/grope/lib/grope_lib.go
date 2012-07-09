e5570115d27b7bd72f197889b74794b8f08d1dd2
Aaron Hamid <aaron.hamid@gmail.com>
2012-07-08 20:34:59 -0400

    fix struct passing, logging, tests

--- src/grope/lib/grope_lib.go
+++ src/grope/lib/grope_lib.go
@@ -54,15 +54,18 @@ func New(program string) *Grope {
   return &grope
 }
 
-func (grope Grope) PrintHelpAndExit() {
+func (grope *Grope) PrintHelpAndExit() {
   fmt.Fprintf(os.Stderr, "Usage: %s [-d] [-i|-I] <regex> [replacement] [file file file]\n", grope.program)
   fmt.Fprintf(os.Stderr, "       %s --help\n", grope.program)
   grope.flagSet.PrintDefaults()
   os.Exit(BAD_ARGS)
 }
 
-func (grope Grope) ParseArgs(args []string) {
-  grope.flagSet.Parse(args)
+func (grope *Grope) ParseArgs(args []string) {
+  err := grope.flagSet.Parse(args)
+  if err != nil {
+    fail(err.Error(), BAD_ARGS)
+  }
 
   args = grope.flagSet.Args()
 
@@ -77,15 +80,17 @@ func (grope Grope) ParseArgs(args []string) {
   }
 
   grope.re = regexp.MustCompile(args[0])
-  grope.replacement, grope.files = parse_file_args(args[1:])
+
+  grope.replacement, grope.files = grope.parseFileArgs(args[1:])
+
 }
 
-func (grope Grope) Main(args []string) {
+func (grope *Grope) Main(args []string) {
   grope.ParseArgs(args)
   grope.Exec()
 }
 
-func (grope Grope) Exec() {
+func (grope *Grope) Exec() {
   if grope.replacement != nil {
     grope.log.Printf("Replacement string: %s", *grope.replacement)
   }
@@ -110,7 +115,7 @@ func (grope Grope) Exec() {
   }
 }
 
-func (grope Grope) GropeFile(file string, read_func ReadFunc) {
+func (grope *Grope) GropeFile(file string, read_func ReadFunc) {
   input, err := read_func()
   handle_file_error(err, "Error reading file", file, INPUT_ERR)
 
@@ -131,7 +136,7 @@ func (grope Grope) GropeFile(file string, read_func ReadFunc) {
 }
 
 
-func (grope Grope) Replace(input []byte, file string, out *os.File) *os.File {
+func (grope *Grope) Replace(input []byte, file string, out *os.File) *os.File {
   grope.log.Printf("Processing input file: %s", file)
   if (grope.inplace) {
     if (os.Stdin.Name() == file) {
@@ -146,14 +151,14 @@ func (grope Grope) Replace(input []byte, file string, out *os.File) *os.File {
   return out
 }
 
-func (grope Grope) Find(input []byte, file string, out *os.File) {
+func (grope *Grope) Find(input []byte, file string, out *os.File) {
   result := grope.re.FindAll(input, MAX_INT)
   for _, match := range result {
     write_match(out, grope.with_filename, file, match)
   }
 }
 
-func (grope Grope) Expand(input []byte, file string, out *os.File) {
+func (grope *Grope) Expand(input []byte, file string, out *os.File) {
   result := grope.re.FindAllSubmatchIndex(input, MAX_INT)
   for _, match := range result {
     dst := []byte{}
@@ -174,30 +179,30 @@ func handle_file_error(err error, file string, msg string, code int) {
   }
 }
 
-func is_file(file string) bool {
+func (grope *Grope) isFile(file string) bool {
   isfile := false
   info, err := os.Stat(file)
   if err != nil {
     if os.IsNotExist(err) {
-      log.Print("No such file: " + file)
+      grope.log.Print("No such file: " + file)
     } else {
-      log.Print(err.Error())
+      grope.log.Print(err.Error())
     }
   } else if info.IsDir() {
-    log.Print(file + " is not a file")
+    grope.log.Print(file + " is not a file")
   } else {
     isfile = true
   }
   return isfile
 }
 
-func parse_file_args(args []string) (replacement *string, files []string) {
+func (grope *Grope) parseFileArgs(args []string) (replacement *string, files []string) {
   replacement = nil
   files = args
   if len(args) > 0 {
-    if !is_file(args[0]) {
+    if !grope.isFile(args[0]) {
       replacement = &args[0]
-      log.Printf("Interpreting '%s' as replacement string", *replacement)
+      grope.log.Printf("Interpreting '%s' as replacement string", *replacement)
       files = args[1:]
     }
   }
--- src/grope/lib/grope_lib_test.go
+++ src/grope/lib/grope_lib_test.go
@@ -5,20 +5,20 @@ import "os"
 
 func TestDirIsNotFile(test *testing.T) {
   path, _ := os.Getwd()
-  if (is_file(path)) {
+  if (New("test").isFile(path)) {
     test.Error("Directory detected as file")
   }
 }
 
 func TestMissingIsNotFile(test *testing.T) {
-  if (is_file("i don't exist")) {
+  if (New("test").isFile("i don't exist")) {
     test.Error("Non-existent file detected as file")
   }
 }
 
 func TestParseValidFileArgs(test *testing.T) {
   args := []string{"../../../test/test1.txt", "../../../test/test2.txt"}
-  replacement, files := parse_file_args(args)
+  replacement, files := New("test").parseFileArgs(args)
   if replacement != nil {
     test.Error("first file interpreted as replacement string")
   }
@@ -31,7 +31,7 @@ func TestParseValidFileArgs(test *testing.T) {
 
 func TestParseFileArgsWithReplacement(test *testing.T) {
   args := []string{"abcd", "../../../test/test2.txt"}
-  replacement, files := parse_file_args(args)
+  replacement, files := New("test").parseFileArgs(args)
   if replacement == nil || *replacement != "abcd" {
     test.Error("first file not interpreted as replacement string")
   }
